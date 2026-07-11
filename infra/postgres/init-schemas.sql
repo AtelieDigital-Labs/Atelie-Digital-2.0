79e4938d46ead5ddefc321d69bad0e2b845c19a5
Aroldo <aroldo.caval@gmail.com>
2026-07-11 06:06:15 -0300

    feat: add minio docker image

--- infra/postgres/init-schemas.sql
+++ infra/postgres/init-schemas.sql
@@ -2,8 +2,10 @@
 CREATE SCHEMA IF NOT EXISTS accounts;
 CREATE SCHEMA IF NOT EXISTS catalogs;
 CREATE SCHEMA IF NOT EXISTS orders;
+CREATE SCHEMA IF NOT EXISTS logs;
 
 -- Se o usuário padrão não for o superuser, garante as permissões (opcional)
 ALTER SCHEMA accounts OWNER TO atelie;
 ALTER SCHEMA catalogs OWNER TO atelie;
 ALTER SCHEMA orders OWNER TO atelie;
+ALTER SCHEMA logs OWNER TO atelie;
